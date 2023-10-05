2a02a930d63a7432d262a4a6f3047331bc96cd49
slbabin <>
2023-10-05 20:46:56 +0000

    Initial commit. Installed Django, created model

---
+++ shoppinglistapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Item(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    complete = models.BooleanField(null=False, blank=False, default=False)
+
+
+    def __str__(self):
+        return self.name
+
+
